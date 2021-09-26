b4c38b092f177928c58da69c67b214f70c63a944
R-Tim <R-Tim@DESKTOP-CIIBS6G>
2021-09-27 01:18:17 +0300

    added Junit tests

--- src/test/java/ua/com/rtim/charcounter/CachedCharCounterTest.java
+++ src/test/java/ua/com/rtim/charcounter/CachedCharCounterTest.java
@@ -17,37 +17,33 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 class CachedCharCounterTest {
 
-	private Map<String, Map<Character, Integer>> cache = new LinkedHashMap<>();
-	private Map<Character, Integer> expected = new LinkedHashMap<>();
-
 	@Mock
-	CharCounter charCounter;
+	private CachedCharCounter cachedCharCounter;
 
 	@Test
 	void givenSomeNonLetterChars_whenCountChars_thenAmountChars() {
-		when(charCounter.countChars("he20lllo0")).thenReturn(cache.get("he20lllo0"))
-				.thenThrow(IllegalArgumentException.class);
+		when(cachedCharCounter.countChars("he20lllo0")).thenReturn(null).thenThrow(IllegalArgumentException.class);
+		Map<Character, Integer> expected = new LinkedHashMap<>();
 		expected.put('h', 1);
 		expected.put('e', 1);
 		expected.put('2', 1);
 		expected.put('0', 2);
 		expected.put('l', 3);
 		expected.put('o', 1);
-		when(charCounter.countChars("he20lllo0")).thenReturn(cache.put("he20lllo0", expected));
-		when(charCounter.countChars("he20lllo0")).thenReturn(cache.get("he20lllo0"));
-		assertEquals(expected, charCounter.countChars("he20lllo0"));
-		assertEquals(expected, charCounter.countChars("he20lllo0"));
-		verify(charCounter, times(2)).countChars("he20lllo0");
+		when(cachedCharCounter.countChars("he20lllo0")).thenReturn(expected);
+		assertEquals(expected, cachedCharCounter.countChars("he20lllo0"));
+		assertEquals(expected, cachedCharCounter.countChars("he20lllo0"));
+		verify(cachedCharCounter, times(2)).countChars("he20lllo0");
 	}
 
 	@Test
 	void givenSimilarChars_whenCountChars_thenAmountOfThisChar() {
-		when(charCounter.countChars("hhhh")).thenReturn(cache.get("hhhh")).thenThrow(IllegalArgumentException.class);
+		when(cachedCharCounter.countChars("hhhh")).thenReturn(null).thenThrow(IllegalArgumentException.class);
+		Map<Character, Integer> expected = new LinkedHashMap<>();
 		expected.put('h', 4);
-		when(charCounter.countChars("hhhh")).thenReturn(cache.put("hhhh", expected));
-		when(charCounter.countChars("hhhh")).thenReturn(cache.get("hhhh"));
-		assertEquals(expected, charCounter.countChars("hhhh"));
-		assertEquals(expected, charCounter.countChars("hhhh"));
-		verify(charCounter, times(2)).countChars("hhhh");
+		when(cachedCharCounter.countChars("hhhh")).thenReturn(expected);
+		assertEquals(expected, cachedCharCounter.countChars("hhhh"));
+		assertEquals(expected, cachedCharCounter.countChars("hhhh"));
+		verify(cachedCharCounter, times(2)).countChars("hhhh");
 	}
 }
--- src/test/java/ua/com/rtim/charcounter/UniqueCharCounterTest.java
+++ src/test/java/ua/com/rtim/charcounter/UniqueCharCounterTest.java
@@ -11,16 +11,37 @@ import org.junit.jupiter.api.Test;
 class UniqueCharCounterTest {
 
 	private UniqueCharCounter uniqueCharCounter = new UniqueCharCounter();
-	private Map<Character, Integer> expected = new LinkedHashMap<>();
 
 	@Test
-	void givenOnlySpaces_whenCountChars_thenAmountOfSpaces() {
-		expected.put(' ', 3);
-		assertEquals(expected, uniqueCharCounter.countChars("   "));
+	void givenEmptyString_whenCountChars_thenEmptyString() {
+		Map<Character, Integer> expected = new LinkedHashMap<>();
+		assertEquals(expected, uniqueCharCounter.countChars(""));
+	}
+
+	@Test
+	void givenOnlySpace_whenCountChars_thenSingleSpace() {
+		Map<Character, Integer> expected = new LinkedHashMap<>();
+		expected.put(' ', 1);
+		assertEquals(expected, uniqueCharCounter.countChars(" "));
+	}
+
+	@Test
+	void givenTwoSpaces_whenCountChars_thenAmountOfSpacesIsTwo() {
+		Map<Character, Integer> expected = new LinkedHashMap<>();
+		expected.put(' ', 2);
+		assertEquals(expected, uniqueCharCounter.countChars("  "));
+	}
+
+	@Test
+	void givenFiveSpaces_whenCountChars_thenAmountOfSpacesIsFive() {
+		Map<Character, Integer> expected = new LinkedHashMap<>();
+		expected.put(' ', 5);
+		assertEquals(expected, uniqueCharCounter.countChars("     "));
 	}
 
 	@Test
 	void givenSomeNonLetterChars_whenCountChars_thenAmountOfChars() {
+		Map<Character, Integer> expected = new LinkedHashMap<>();
 		expected.put('h', 1);
 		expected.put('e', 1);
 		expected.put('2', 1);
@@ -28,6 +49,38 @@ class UniqueCharCounterTest {
 		expected.put('l', 3);
 		expected.put('o', 1);
 		assertEquals(expected, uniqueCharCounter.countChars("he20lllo0"));
+	}
+
+	@Test
+	void givenLettersInMixedCase_whenCountChars_thenAmountOfChars() {
+		Map<Character, Integer> expected = new LinkedHashMap<>();
+		expected.put('m', 2);
+		expected.put('K', 3);
+		expected.put('f', 3);
+		expected.put('M', 3);
+		expected.put('O', 3);
+		expected.put('o', 2);
+		expected.put('F', 2);
+		expected.put(' ', 2);
+		expected.put('k', 3);
+		assertEquals(expected, uniqueCharCounter.countChars("mKfMOfKoF KOkfMokF MkmO"));
+	}
+
+	@Test
+	void givenSomeNonLetterCharsInMixedCase_whenCountChars_thenAmountOfChars() {
+		Map<Character, Integer> expected = new LinkedHashMap<>();
+		expected.put('Y', 1);
+		expected.put('4', 1);
+		expected.put('a', 2);
+		expected.put('!', 3);
+		expected.put('j', 3);
+		expected.put('E', 2);
+		expected.put('e', 1);
+		expected.put('m', 1);
+		expected.put('A', 2);
+		expected.put('T', 1);
+		expected.put('1', 1);
+		assertEquals(expected, uniqueCharCounter.countChars("Y4a!jEeE!amAT!j1jA"));
 	}
 
 	@Test
