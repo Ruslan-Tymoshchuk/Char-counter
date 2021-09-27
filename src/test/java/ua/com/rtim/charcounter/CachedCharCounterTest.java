18a4d027717170d2fca6e5dabdcbdf5d05664419
R-Tim <R-Tim@DESKTOP-CIIBS6G>
2021-09-27 15:27:28 +0300

    Addjusted Junit tests with Mokito

--- src/test/java/ua/com/rtim/charcounter/CachedCharCounterTest.java
+++ src/test/java/ua/com/rtim/charcounter/CachedCharCounterTest.java
@@ -10,19 +10,21 @@ import java.util.Map;
 
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
 
 @ExtendWith(MockitoExtension.class)
-
 class CachedCharCounterTest {
 
 	@Mock
-	private CachedCharCounter cachedCharCounter;
+	private CharCounter сharCounter;
+	@InjectMocks
+	private CachedCharCounter cachedCharCounter = new CachedCharCounter(сharCounter);
 
 	@Test
 	void givenSomeNonLetterChars_whenCountChars_thenAmountChars() {
-		when(cachedCharCounter.countChars("he20lllo0")).thenReturn(null).thenThrow(IllegalArgumentException.class);
+		when(сharCounter.countChars("he20lllo0")).thenReturn(null).thenThrow(IllegalArgumentException.class);
 		Map<Character, Integer> expected = new LinkedHashMap<>();
 		expected.put('h', 1);
 		expected.put('e', 1);
@@ -30,20 +32,20 @@ class CachedCharCounterTest {
 		expected.put('0', 2);
 		expected.put('l', 3);
 		expected.put('o', 1);
-		when(cachedCharCounter.countChars("he20lllo0")).thenReturn(expected);
+		when(сharCounter.countChars("he20lllo0")).thenReturn(expected);
 		assertEquals(expected, cachedCharCounter.countChars("he20lllo0"));
 		assertEquals(expected, cachedCharCounter.countChars("he20lllo0"));
-		verify(cachedCharCounter, times(2)).countChars("he20lllo0");
+		verify(сharCounter, times(1)).countChars("he20lllo0");
 	}
 
 	@Test
 	void givenSimilarChars_whenCountChars_thenAmountOfThisChar() {
-		when(cachedCharCounter.countChars("hhhh")).thenReturn(null).thenThrow(IllegalArgumentException.class);
+		when(сharCounter.countChars("hhhh")).thenReturn(null).thenThrow(IllegalArgumentException.class);
 		Map<Character, Integer> expected = new LinkedHashMap<>();
 		expected.put('h', 4);
-		when(cachedCharCounter.countChars("hhhh")).thenReturn(expected);
+		when(сharCounter.countChars("hhhh")).thenReturn(expected);
 		assertEquals(expected, cachedCharCounter.countChars("hhhh"));
 		assertEquals(expected, cachedCharCounter.countChars("hhhh"));
-		verify(cachedCharCounter, times(2)).countChars("hhhh");
+		verify(сharCounter, times(1)).countChars("hhhh");
 	}
 }
